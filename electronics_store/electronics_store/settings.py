5f496c340282fabe8f3016311c232a978466d5f0
qweasd22 <koltsov-main@mail.ru>
2026-05-06 10:11:34 +0300

    123

--- electronics_store/electronics_store/settings.py
+++ electronics_store/electronics_store/settings.py
@@ -161,10 +161,10 @@ LOGOUT_REDIRECT_URL = '/'  # куда пользователь идет посл
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
